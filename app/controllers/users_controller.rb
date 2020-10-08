f064c0f8b0979fc7af67271524f885d78365ec8c
Rinko Yabiku <wta15214@nuhw.ac.jp>
2020-10-08 19:57:02 +0900

    Hello_Worldアプリ

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def index
+    @greet = "Hello World!!"
+  
+  # Userモデルをインスタンス化
+  user = User.new
+  # introduceメソッドを実行
+  @my_introduce = user.introduce
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User
+  def initialize
+    @first_name = "Yabiku"
+    @last_name = "Rinko"
+    @birthday = "1996/12/18"
+    @age = 23
+    @birthplace = "Okinawa/ginowansi"
+    @hobby = "soccer"
+  end
+  def introduce
+    <<~EOS
+
+    私の名前は#{@first_name + @last_name}です。
+    誕生日は#{@birthday}で、年齢は#{@age}歳。
+    出身地は#{@birthplace}で、趣味は#{@hobby}です。
+
+    EOS
+  end
+end
